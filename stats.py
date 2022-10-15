884746213a5ba763990442174eba979abd7508d4
maj <majs.1236@gmail.com>
2022-10-15 13:31:01 +0200

    made player_stats a function so it gets shuffled if called

--- stats.py
+++ stats.py
@@ -1,6 +1,8 @@
-from math import radians
 from random import randint
-#player = input(str("what is your name? "))
-#
-player_stats = [randint(10,100), randint(10,100), randint(1,5)]
-print(player_stats)
+player = input(str("what is your name? "))
+#this will randomize Player_stats
+def stats():
+    global player_stats
+    player_stats = [randint(10,100), randint(10,100), randint(1,5)]
+    return(player_stats)
+
