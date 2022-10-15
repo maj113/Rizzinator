43369186fcb0257c40aebc8226ac58f204ed654c
maj <majs.1236@gmail.com>
2022-10-15 15:05:12 +0200

    placed woman_intro in a subfolder called stories, created file for Mia's stories

--- datesim_0.0.2.py
+++ datesim_0.0.2.py
@@ -1,7 +1,6 @@
 #Rizzsim_0.0.2
 #KrpanKodingKompany, All rights reserved 2022
-
-from woman_intro import miaIntr, emmaIntr, lanaIntr, janeIntr
+from stories import woman_intro as wi, MiaStories as ms
 from stats import player, stats
 from time import sleep
 from random import choice
@@ -14,26 +13,26 @@ woman = (choice(woman_list))
 def interactions():
     if (woman == "Mia"):
         if stats() >= [20,25,2]: 
-            print(miaIntr)
+            print(wi.miaIntr)
             sleep(10)
             accept_mia = input(f"do you wish to continue Mia's story? If so, type yes ")
             if (accept_mia == "yes" or "y"):
-                print('you are walking down the hallway on your first day of college, when you look behind you and spot one of if not the most beautiful girl you have ever seen, walking behind you. You just can\'t take your eyes off of her and because you are walking with your head turned backwards, you bump into some poor guy who was just minding his buisness. Everything you are holding falls on the ground and you scramble to pick it all up. Suddenly a soft angelic voice asks you "hey, may I help you?" it\'s her! The beautifull tall motherly girl that caused you to drop all of your stuff. you become so flustered that you can\'t answer, watching as she picks your stuff up into a neat pile. "Here you go" she says with a smile on her stunning face. Before you have the chance to say anything she leaves just as the bell rings.')
+                print(ms.MiaStory1)
                 sleep(10)            
             else: interactions()
         else: interactions() 
 """
     elif (woman == "Emma"):
         if (looks>34, jacked>25, attraction>3):
-            print(emmaIntr)
+            print(wi.emmaIntr)
         else: interactions()
     elif (woman == "Lana"):
         if (looks>30, jacked>25, attraction>3):
-            print(lanaIntr)
+            print(wi.lanaIntr)
         else: interactions()
     elif (woman == "Jane"):
         if (looks>25, jacked>20, attraction>2):
-            print(janeIntr)
+            print(wi.janeIntr)
         else: interactions()
     elif (woman == "Jenny"):
         print("Jenny: true")
