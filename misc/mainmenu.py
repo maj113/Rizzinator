14a96ae3eeec6d881765e2cd154051f3d95fa4af
maj <majs.1236@gmail.com>
2022-10-16 17:52:29 +0200

    test remove commit, checking if shit got saved correctly

--- misc/mainmenu.py
+++ misc/mainmenu.py
@@ -1,14 +1,15 @@
 from random import randint
-from inspect import getmembers, isfunction
 from gameplay import mia
 from time import sleep
 from misc.gym import gymExcercise
 from misc.plstats import player_stats
 story_list = []
 menuchoices = ("Hit the gym [1]\n ", "Exit the game [2]\n","Check current stats [3]\n","Play story from beginning [4]\n"," Continue story [5]")
+
 def gamemenu():
     print("What do you want to do:\n " , *menuchoices)
     gamechoice = input("Input the number next to the action you want to do ")
+    
     if  gamechoice == "1":
         player_stats[1] = (player_stats[1]+randint(0,2))
         gymExcercise()
@@ -45,5 +46,4 @@ def gamemenu():
                 savestate()
             else:
                 print("where do you want to continue?")
-                print(getmembers(mia, isfunction))
         savestate()
