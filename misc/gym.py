1d49690e3bea5b79cb82dda6ceaf33dc75726e94
Gorchii Gorazd <djslamnik@gmail.com>
2022-10-16 10:00:36 +0200

    working on the gym update

---
+++ misc/gym.py
@@ -0,0 +1,11 @@
+#random comment cause this is what devs do... right?
+
+from time import sleep
+from random import randint
+from misc.mainmenu import player_stats
+from misc.gymTexts import arivingAtGym
+
+
+def gymExcercise():
+    print(arivingAtGym)
+    
