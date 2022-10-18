468cdbf952a6fb3d5693747cf07081f4fce69e57
maj <majs.1236@gmail.com>
2022-10-18 16:56:58 +0200

    removed

--- misc/gym.py
+++ misc/gym.py
@@ -5,13 +5,12 @@ from random import randint
 
 def gymExcercise():
     from misc.mainmenu import gamemenu
-    from misc.mainmenu import slow_print
     from misc.plstats import player_stats
-    slow_print("\nYou packed your gym shorts, protein shake and headed to your local gym.")
-    print("\nYour current stats are: \n","Looks =",player_stats[0],"\n Jacked =",player_stats[1],"\n Attraction =",player_stats[2],"")
+    print("\nYou packed your gym shorts, protein shake and headed to your local gym.")
+    print("\nYour current stats are: \n\n","Looks =",player_stats[0],"\n Jacked =",player_stats[1],"\n Attraction =",player_stats[2],"\n")
     excerciseTime = int(input("\nhow long do you wish to excercise for? Use seconds: "))
     if excerciseTime > 20:
-        slow_print("\nyou can not excercise for more than 20 seconds!")
+        print("\nyou can not excercise for more than 20 seconds!")
         gymExcercise()
     else:
         player_stats[1] = player_stats[1] + excerciseTime
@@ -19,8 +18,9 @@ def gymExcercise():
             print("💪", flush = True)
             sleep(1)
         print()
+        sleep(excerciseTime)
         print("\nyou got closer to being an absolute CHAD, good work chap. Your new stats are\n")
-        print("Looks=",player_stats[0],"\n Jacked=",player_stats[1],"\n Attraction=",player_stats[2],"\n")
+        print("\nYour current stats are: \n\n","Looks=",player_stats[0],"\n Jacked=",player_stats[1],"\n Attraction=",player_stats[2],"\n")
         gamemenu()
     
   
