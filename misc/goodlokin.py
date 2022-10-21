d0976301a3a6575b5ecea5b1faf03d6869da1e4c
maj <majs.1236@gmail.com>
2022-10-21 21:17:06 +0200

    fixed circular import, fixed spelling in goodlokin

--- misc/goodlokin.py
+++ misc/goodlokin.py
@@ -1,14 +1,12 @@
 #coment cuz python dev :sunglasses:
-
 from time import sleep
-import misc.mainmenu as mm
 from misc.plstats import player_stats
-from mainmenu import slow_print
 
 def goingToShop():
+    from misc.mainmenu import slow_print, gamemenu 
     slow_print("\nYou go shoping for perfume") 
     perfumes = ["\nChallen", "Bugo Hoss", "Rakija", "Juzza"]
-    slow_print(*perfumes)
+    slow_print(*perfumes, speed=5)
     perfume_picked = input("\nWhich one of these perfumes do you wish to purchase? ").lower()
 
     if perfume_picked == "challen":
@@ -29,9 +27,9 @@ def goingToShop():
     elif perfume_picked == "juzza":
         sleep(1)
         player_stats[2] = player_stats[2] + 1
-        slow_print("\nYou took a look at the Juzza bottle and asked arround if anyone knew what the hell Juzza is. Much to your dissapointment no one knew what it was and the cashier said it isn't even in their inventory. Nevertheless you now smell like raspberies and creampie.", player_stats[2])
+        slow_print("\nYou took a look at the Juzza bottle and asked around if anyone knew what the hell Juzza is. Much to your dissapointment no one knew what it was and the cashier said it isn't even in their inventory. Nevertheless you now smell like raspberies and creampie.", player_stats[2])
     
     else:
         slow_print("\nYou ventured to the very back of the shop, picked out a nice bottle of perfume, walked up to the cashier, took out your lighter and in a matter of seconds created a makeshift flamethrower, with which you melted the poor cashiers face off in an instant. Try again")
         sleep(1)
-        mm.gamemenu()
+        gamemenu()
